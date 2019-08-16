52cd7264196c407f42281bcca8ac71f64319e7b8
Lucky Hlakaza <nhlalucky9@gmail.com>
2019-08-16 14:19:48 +0200

    refactor(flex): display items in flex

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,29 @@ declare const $: any;
 export class AppComponent implements AfterViewInit {
   title = 'circularNav';
   type = 1;
-  radius = '20em'; // distance from center
+  radius = '12em'; // distance from center
   start = -180; // shift start from 0
-
+  isOpen = false;
   constructor() {}
-
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
   ngAfterViewInit() {
     const elements = document.querySelectorAll('.menu-wraper .menu-item');
     if (elements.length > 0) {
-      const numberOfElements = (this.type === 1) ?
-      elements.length : elements.length - 1; // adj for even distro of elements when not full circle
-      const slice = 360 * this.type / numberOfElements;
-      console.log(elements);
+      // adj for even distro of elements when not full circle
+      const numberOfElements = this.type === 1 ? elements.length : elements.length - 1;
+      const slice = (360 * this.type) / numberOfElements;
       elements.forEach((item, index) => {
         const rotate = slice * index + this.start;
         const rotateReverse = 180;
-        item.setAttribute('style', `transform: rotate(${rotate}deg) translate(${this.radius}) rotate(${rotateReverse}deg)`);
-     });
+        item.setAttribute(
+          'style',
+          `transform: rotate(${rotate}deg)
+          translate(${this.radius})
+          rotate(${rotateReverse}deg)`
+        );
+      });
     }
-
   }
 }
